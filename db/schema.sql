6a6c49b4929b950480fda52ecb8a35a05df2f9b7
Stet <solaye1988@gmail.com>
2019-07-16 10:56:21 -0600

    hope it works

--- db/schema.sql
+++ db/schema.sql
@@ -2,7 +2,7 @@ CREATE DATABASE burgers_db;
 USE burgers_db;
 
 CREATE TABLE cats
-(
+(	createdAt TIMESTAMP NOT NULL,
 	id int NOT NULL AUTO_INCREMENT,
 	burger_name varchar(255) NOT NULL,
 	devoured BOOLEAN DEFAULT false,
